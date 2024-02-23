4079e67223205b4f54d3ce9b34955dd7e2a97664
Arthur Badu Reis <143804510+Atu141@users.noreply.github.com>
2024-02-23 11:53:53 -0300

    Add files via upload

---
+++ Orientação a Objetos/Atu/Aula_03/src/Aula03/petshop/model/Cachorro.java
@@ -0,0 +1,31 @@
+package Aula03.petshop.model;
+
+public class Cachorro {
+	
+	//Atributos
+	private String raca;
+	
+	private String nome;
+
+	private String porte;
+	
+	
+	//Construtor
+	public Cachorro(String size, String type, String name) {
+		raca = type;
+		porte = size;
+		nome = name;
+
+	}
+	
+	//Metodos
+	public void latir() {
+		
+	}
+	
+public void comer() {
+		
+	}
+
+
+}
